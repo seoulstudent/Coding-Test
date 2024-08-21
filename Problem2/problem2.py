451320cda692fdecd79ac511e69e95d627d1a9c5
seoulstudent <59911052+seoulstudent@users.noreply.github.com>
2024-08-22 03:26:16 +0900

    Update problem2.py

--- Problem2/problem2.py
+++ Problem2/problem2.py
@@ -1,1 +1,18 @@
+class Calc:
+    def __init__(self, value):
+        self.value = value
 
+    def add(self, value):
+        self.value += value
+        return self
+
+    def subtract(self, value):
+        self.value -= value
+        return self
+
+    def out(self):
+        return self.value
+
+calculator = Calc(0)
+result = calculator.add(4).add(5).subtract(3).out()
+print(result)
